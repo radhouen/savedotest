3febf927a228b2e113f74381e51de466a4b11380
assakra radhouen <askriradhouen09@gmail.com>
2018-03-27 19:52:56 +0100

    Test the Api with mocha

--- my-project/test/test.js
+++ my-project/test/test.js
@@ -1,15 +1,30 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+var supertest = require("supertest");
+var should = require("should");
+
+// This agent refers to PORT where program is runninng.
+
+var server = supertest.agent("https://api.github.com/users/octocat/repos");
+
+// UNIT test begin
+
+describe("SAMPLE unit test",function(){
+
+  // #1 should return home page
+
+  it("should return all issus from github",function(done){
+
+    // calling home page api
+    server
+    .get("/")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      // Error key should be false.
+      res.body.error.should.equal(false);
+      done();
     });
   });
-});var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
+
 });
